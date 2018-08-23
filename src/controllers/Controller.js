0f95c8a53432a774d440ad0590edbc4a19ac37c5
binh <lbinh@yahoo-corp.jp>
2018-08-23 17:14:43 +0700

    valid

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -1,6 +1,9 @@
 'use strict';
 import Mail from './../models/Mail';
 import config from './../config/config';
+import errorString from '../languages/Error';
+import { resolve } from 'path';
+import { oneOf } from 'express-validator/check';
 
 /**
  * this is Controller class
@@ -51,31 +54,62 @@ export default class Controller {
    * @returns {Promise<*>}
    */
   async registerMailToListController(req, res) {
-    /**
-    * @param {Object} values
-    * @returns {JSON}
-    */
- 
-    this.values = !req.body ? "" : req.body;
- 
-    let responseData = {};
- 
-    if(this.values.length < 0 || this.values.length > 100) {
-      responseData.status = config.httpStatus.BAD_REQUEST.status;
-      responseData.code = errorString.codeOutOfRange;
-      responseData.message = errorString.messageOutOfRange;
-      return res.status(responseData.status).json(responseData)
-    }else {
-      
-    }
-   }
- 
    /**
-    * this is jsonValidation method
-    * @param {String} values - values Object
-    * @returns {Object}
-    */
-   jsonValidation(values) {
-     return JSON.parse(values);
+   * @param {Object} values
+   * @returns {JSON}
+   */
+
+   const data = !req.body ? "" : req.body;
+
+   let responseData = {};
+
+   if(data.length < 0 || data.length > 100) {
+     responseData.status = config.httpStatus.BAD_REQUEST.status;
+     responseData.code = errorString.codeOutOfRange;
+     responseData.message = errorString.messageOutOfRange;
+     return res.status(responseData.status).json(responseData)
+   }else {
+    //make type object of body request folow rule of express validator
+    const objectData = { data };
+    req.body = objectData;
+
+    // Validation name is string and no empty
+    req.checkBody('data.*.name')
+    .not()
+    .isEmpty()
+    .withMessage('name-require')
+    .trim()
+    .isString()
+    .withMessage('name-format');
+    
+    // Validation address is string, no empty, email format
+    req.checkBody('data.*.address')
+    .not()
+    .isEmpty()
+    .withMessage('address-require')
+    .isEmail()
+    .withMessage('address-format')
+    .trim()
+    .normalizeEmail()
+    .withMessage('address-format')
+    .isString()
+    .withMessage('address-format');
+
+    const errors = req.validationErrors();
+    
+    if(errors.length > 0) {
+      return res.json({ errors: errors});
+    }
+    return res.json('ok');
    }
+  }
+
+  /**
+   * this is jsonValidation method
+   * @param {String} values - values Object
+   * @returns {Object}
+   */
+  jsonValidation(values) {
+    return JSON.parse(values);
+  }
 }
